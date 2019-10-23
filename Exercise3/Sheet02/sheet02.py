dcfc31ef8fbe46f5fdd0f3c3508d33678140b24c
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-10-23 22:05:16 +0200

    Created NCC (but not working)

--- Exercise3/Sheet02/sheet02.py
+++ Exercise3/Sheet02/sheet02.py
@@ -5,6 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 np.set_printoptions(threshold=sys.maxsize)
 
+
 def display_image(window_name, img):
     """
         Displays image with given window name.
@@ -14,6 +15,7 @@ def display_image(window_name, img):
     cv2.imshow(window_name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def mean_absolute_difference(img1, img2):
     """
@@ -28,6 +30,7 @@ def mean_absolute_difference(img1, img2):
 
     return np.absolute(mean_img1 - mean_img2)
 
+
 def get_convolution_using_fourier_transform(image, kernel):
     # Compute FFT of image
     # and shift 0 frequency
@@ -35,7 +38,8 @@ def get_convolution_using_fourier_transform(image, kernel):
     ftimage = np.fft.fft2(image)
     ftshift = np.fft.fftshift(ftimage)
     magnitude_spectrum = 20 * np.log(np.abs(ftshift))
-    magnitude_spectrum = np.asarray(magnitude_spectrum, dtype=np.uint8)
+    magnitude_spectrum = np.asarray(
+        magnitude_spectrum, dtype=np.uint8)
     display_image('Task1: Magn. Spect', magnitude_spectrum)
 
     # Blur image with kernel
@@ -46,52 +50,112 @@ def get_convolution_using_fourier_transform(image, kernel):
     # matrix
     return np.abs(f_blur)
 
+
 def task1():
     # Read image
     image = cv2.imread("./data/einstein.jpeg", 0)
-    display_image('Task1: Image', image)
+    # display_image('Task1: Image', image)
 
     # Get gaussian kernel
     kernel = cv2.getGaussianKernel(7, 1)
 
     # Convolute image gaussian kernel
     conv_result = cv2.filter2D(image, -1, kernel)
-    display_image('Task1: Blur kernel', conv_result)
+    # display_image('Task1: Blur kernel', conv_result)
 
     # Convolute image using FFT
-    fft_result = get_convolution_using_fourier_transform(image, kernel)
+    # fft_result = get_convolution_using_fourier_transform(
+    #     image, kernel)
     # display_image('Task1: FFT', fft_result)
 
     # compare results
-    print("(Task1) - Mean absolute difference: ", mean_absolute_difference(conv_result, fft_result))
+    # print("(Task1) - Mean absolute difference: ",
+    # mean_absolute_difference(conv_result, fft_result))
+
 
 def sum_square_difference(image, template):
     return None
 
+
 def normalized_cross_correlation(image, template):
-    return None
+    # resizing template to square size
+    # height, width = template.shape
+    # if height > width:
+    #     pad_h = (height - width) // 2
+    #     template = template[pad_h: pad_h + width, 0: width]
+    # else:
+    #     pad_w = (width - height) // 2
+    #     template = template[0: height, pad_w: pad_w + height]
+
+    height, width = template.shape
+    h2 = (height - 1) // 2
+    w2 = (width - 1) // 2
+    print(image.shape)
+    # insert a border
+    image = cv2.copyMakeBorder(
+        image, h2, h2, w2, w2, cv2.BORDER_CONSTANT, value=0).astype(np.int64)
+    img_norm = image.copy()
+
+    # kernel mean
+    k_sum = 0
+    for y in range(height):
+        for x in range(width):
+            k_sum += template[y, x]
+    k_mean = k_sum / (height*width)
+
+    for y in range(h2, image.shape[0] - h2):
+        print(y)
+        for x in range(w2, image.shape[1] - w2):
+            # image window mean
+            i_sum = 0
+            i_sum = np.sum(
+                image[(y - h2): ((y + h2) + 1), (x - w2): ((x + w2) + 1)])
+            i_mean = i_sum / (height*width)
+
+            # get the NCC value
+            num, denum, dG, dF = 0, 0, 0, 0
+            for k in range(-h2, h2 + 1):
+                for l in range(-w2, w2 + 1):
+                    dg = template[k + h2, l + w2] - k_mean
+                    df = image[y + k, x + l] - i_mean
+                    num += dg * df
+                    dG += pow(dg, 2)
+                    dF += pow(df, 2)
+
+            denum = pow((dG * dF), 0.5)
+            val = num / denum
+            img_norm[y, x] = val
+
+    img_norm = (((img_norm - img_norm.min()) /
+                 (img_norm.max() - img_norm.min())) * 255).astype(np.uint8)
+    return img_norm.astype(np.uint8)
+
 
 def task2():
     image = cv2.imread("./data/lena.png", 0)
     template = cv2.imread("./data/eye.png", 0)
-
+    display_image('Image', image)
     result_ssd = sum_square_difference(image, template)
     result_ncc = normalized_cross_correlation(image, template)
-
+    display_image('NCC', result_ncc)
     result_cv_sqdiff = None  # calculate using opencv
     result_cv_ncc = None  # calculate using opencv
 
     # draw rectangle around found location in all four results
     # show the results
 
+
 def build_gaussian_pyramid_opencv(image, num_levels):
     return None
+
 
 def build_gaussian_pyramid(image, num_levels, sigma):
     return None
 
+
 def template_matching_multiple_scales(pyramid, template):
     return None
+
 
 def task3():
     image = cv2.imread("./data/traffic.jpg", 0)
@@ -105,8 +169,10 @@ def task3():
 
     # show result
 
+
 def get_derivative_of_gaussian_kernel(size, sigma):
     return None, None
+
 
 def task4():
     image = cv2.imread("./data/einstein.jpeg", 0)
@@ -122,8 +188,10 @@ def task4():
     cv2.imshow("Magnitude", magnitude)
     cv2.imshow("Direction", direction)
 
+
 def l2_distance_transform_2D(edge_function, positive_inf, negative_inf):
     return None
+
 
 def task5():
     image = cv2.imread("./data/traffic.jpg", 0)
