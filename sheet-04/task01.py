063826e85bcad4b4d75095a6f3116884114a1886
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-11-06 18:54:20 +0100

    Task 1 finished but not working

--- sheet-04/task01.py
+++ sheet-04/task01.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy.linalg as la
 import numpy as np
 import cv2
+import random
 
 
 def plot_snake(ax, V, fill='green', line='red', alpha=1, with_txt=False):
@@ -57,45 +58,116 @@ def get_gradient_image(img):
     return None
 
 
-def get_external(img_gradient, vertices, k_size):
-    matrix_shape = (k_size, vertices.shape[0])
-    U_external = np.zeros(matrix_shape)
+def get_external_w_points(img_gradient, vertices, k_size):
+    """
+    Get the external energy of each point neighbor of each
+    vertices from the gradient image (with respective
+    coordinates of the points).
+    :param img_gradient:
+    :param vertices:
+    :param k_size:
+    :return: External energies and respective points.
+    """
+    # total number of k (with a kernel
+    # of 3*3, k_total = 9)
+    k_total = k_size ** 2
+    # half of the kernel size (with a kernel
+    # of 3*3, k_half = 1)
     k_half = (k_size // 2)
+    U_external = np.zeros((k_total, vertices.shape[0]))
+    U_points = np.zeros((k_total, vertices.shape[0], 2))
+
+    # for each iteration 2, ..., n
     for n in range(vertices.shape[0]):
+        k = 0
         y_vert = vertices[n, 0]
         x_vert = vertices[n, 1]
-        k = 0
         for y_kern in range(-k_half, k_half + 1):
             for x_kern in range(-k_half, k_half + 1):
-                U_external[k, n] = img_gradient[y_vert + y_kern, x_vert + x_kern]
+                y = y_vert + y_kern
+                x = x_vert + x_kern
+                U_points[k, n] = [y, x]
+                U_external[k, n] = - img_gradient[y, x]
                 k += 1
-    return U_external
+    return U_external, U_points
 
 
-def euclidian_distance(a, b):
-    return 0
+def euclidean_distance(a, b):
+    """
+    Calculate the euclidean
+    distance between two point.
+    :param a:
+    :param b:
+    :return: the distance.
+    """
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2) ** 0.5
 
 
-def get_distances(vertices, k_size):
+def get_distances(points_n, points_n_1, k_size, alpha=1):
+    """
+    Get distance between each connection from 1 to n.
+    :param points_n:
+    :param points_n_1:
+    :param k_size: size of the neighbour kernel.
+    :param alpha:
+    :return:
+    """
+    # total number of k (with a kernel
+    # of 3*3, k_total = 9)
     k_total = k_size ** 2
-    k_half = (k_size // 2)
-    Pnk = np.zeros((k_total, k_total))
-    for k in enumerate(range(k_half)):
-        l = 0
+    Pn = np.zeros((k_total, k_total))
+    for k in range(points_n.shape[0]):
+        for l in range(points_n_1.shape[0]):
+            Pn[k, l] = euclidean_distance(points_n[k], points_n_1[l])
 
-    return None
+    return alpha * (Pn - Pn.mean()) ** 2
 
 
-def compute_new_vertices(vertices, U_external, k_size):
-    matrix_shape = (k_size, vertices.shape[0])
+def compute_new_vertices(vertices, U_external, U_points, k_size):
+    """
+    Compute the new vertices of a Snake Active Contours
+    step.
+    :param vertices: np.array of the current vertices.
+    :param U_external: External energy of each point
+    neighbor of each vertices.
+    :param k_size: size of the neighbour kernel.
+    :return: a np.array with a list of all the new
+    vertices of size (n_vertices x coordinates).
+    """
+    # total number of k (with a kernel
+    # of 3*3, k_total = 9)
+    k_total = k_size ** 2
+    matrix_shape = (k_total, vertices.shape[0])
     S_energies = np.zeros(matrix_shape)
     S_paths = np.zeros(matrix_shape)
-    S_energies[:0] = U_external[:, 0]
-    k_total = k_size ** 2
+    S_energies[:, 0] = U_external[:, 0]
+    new_vertices = np.zeros((S_energies.shape[1], 2), dtype=np.int)
+    # for each iteration 2, ..., n
+    # (the first iteration is skipped)
     for n in range(1, vertices.shape[0]):
-        Pnk = get_distances(vertices, k_size)
+        # get distances between n and n-1
+        Pn = get_distances(U_points[:, n], U_points[:, n - 1], k_size)
+        S_n_1 = S_energies[:, n - 1].T
+        Pn = np.add(Pn, S_n_1)
+        min_idxs = np.argmin(Pn, axis=1)
+        min_values = Pn[np.arange(Pn.shape[0]), min_idxs]
+        final_Sn = U_external[:, n].T + min_values
+        S_energies[:, n] = final_Sn.T
+        S_paths[:, n] = min_idxs
 
-    return np.array([[]])
+    # retrieve the final path
+    idx_final_min = np.argmin(S_energies[:, -1])
+    idx_vertices = np.zeros((S_energies.shape[1]), dtype=np.int)
+    idx_vertices[-1] = idx_final_min
+    for n_rev in range(S_paths.shape[1] - 1, 0, -1):
+        idx_vertices[n_rev - 1] = S_paths[idx_vertices[n_rev], n_rev]
+
+    # retrieve final coordinates by indexes
+    for n in range(S_energies.shape[1]):
+        idx_n = idx_vertices[n]
+        new_vertices[n] = U_points[idx_n, n]
+
+    return new_vertices
 
 
 def run(fpath, radius):
@@ -112,16 +184,20 @@ def run(fpath, radius):
     k_size = 3
 
     for t in range(n_steps):
-        U_external = get_external(img_gradient, vertices, k_size)
-        new_vertices = compute_new_vertices(vertices, U_external, k_size)
+        U_external, U_points = get_external_w_points(img_gradient, vertices, k_size)
+        new_vertices = compute_new_vertices(vertices, U_external, U_points, k_size)
         ax.clear()
         ax.imshow(img, cmap='gray')
         ax.set_title('frame ' + str(t))
         plot_snake(ax, new_vertices)
         plt.pause(0.01)
 
-        if new_vertices == vertices:
-            break
+        # if (new_vertices == vertices).all():
+        #     print('found')
+        #     break
+        # else:
+        roll_rand = random.randint(0, new_vertices.shape[0] - 1)
+        vertices = np.roll(new_vertices, roll_rand, axis=0)
 
     plt.pause(2)
 
