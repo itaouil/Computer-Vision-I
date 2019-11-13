fc0ccb6ed3aae96d105628454673674621de126e
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-11-13 19:09:48 +0100

    Compleated task2

--- sheet-04/task02.py
+++ sheet-04/task02.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from matplotlib import rc
-import sys
-np.set_printoptions(threshold=sys.maxsize)
 
 
 # rc('text', usetex=True)  # if you do not have latex installed simply uncomment this line + line 75
@@ -27,6 +25,7 @@ def load_data():
 
     return Im, phi
 
+
 def get_contour(phi):
     """ get all points on the contour
     :param phi:
@@ -40,6 +39,7 @@ def get_contour(phi):
     Y, X = np.nonzero(D)
     return np.array([X, Y]).transpose()
 
+
 def display_image(window_name, img):
     """
         Displays image with given window name.
@@ -49,6 +49,7 @@ def display_image(window_name, img):
     cv2.imshow(window_name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 def grad(g, split=False):
     kernel_x = np.array([[-1, 1]])
@@ -60,16 +61,71 @@ def grad(g, split=False):
     else:
         return np.array([dx, dy])
 
+
+def grad_flipped(g):
+    # Add a border to the top and left
+    border_left = np.zeros((g.shape[0], 1))
+    border_top = np.zeros((g.shape[1]))
+    gx = np.hstack((border_left, g))
+    gy = np.vstack((border_top, g))
+
+    # Computation of the partial derivatives
+    kernel_x = np.array([[-1, 1]])
+    kernel_y = np.array([[-1], [1]])
+    dx = cv2.filter2D(gx.copy(), -1, kernel_x)
+    dy = cv2.filter2D(gy.copy(), -1, kernel_y)
+    return dx[:, :-1], dy[:-1, :]
+
+
+def der_first(g):
+    kernel_x = np.array([[-1, 0, 1]])
+    kernel_y = np.array([[-1], [0], [1]])
+    dx = cv2.filter2D(g.copy(), -1, kernel_x) * (1 / 2)
+    dy = cv2.filter2D(g.copy(), -1, kernel_y) * (1 / 2)
+    return dx, dy
+
+
+def der_sec(g):
+    kernel_x = np.array([[1, -2, 1]])
+    kernel_y = np.array([[1], [-2], [1]])
+    dxx = cv2.filter2D(g.copy(), -1, kernel_x)
+    dyy = cv2.filter2D(g.copy(), -1, kernel_y)
+    return dxx, dyy
+
+
+def der_first_matched(g):
+    kernel = np.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]])
+    dg = cv2.filter2D(g.copy(), -1, kernel) * (1 / 4)
+    return dg
+
+
 def magnitude(dg):
     return np.sqrt(dg[0] ** 2 + dg[1] ** 2)
 
-def compute_curvature(phi):
-    phi_x, phi_y = grad(phi, True)
-    phi_xy = magnitude(np.array([phi_x, phi_y]))
-    phi_xx, phi_yy = grad(phi_xy, True)
 
-    return ((phi_xx * (phi_y ** 2) - 2 * phi_x * phi_y * phi_xy +
-             phi_yy * (phi_x ** 2)) / (phi_x ** 2 + phi_y ** 2))
+def compute_curvature(p):
+    dphi_x, phi_y = der_first(p)
+    dphi_xx, phi_yy = der_sec(p)
+    dphi_xy = der_first_matched(p)
+
+    return ((dphi_xx * (phi_y ** 2) -
+             2 * dphi_x * phi_y * dphi_xy +
+             phi_yy * (dphi_x ** 2)) /
+            (dphi_x ** 2 + phi_y ** 2 + 10 ** (-4)))
+
+
+def compute_propagation(w, phi):
+    dw_x, dw_y = grad(w)
+    max_x = np.where(dw_x < 0, 0, dw_x)
+    max_y = np.where(dw_y < 0, 0, dw_y)
+    min_x = np.where(dw_x < 0, dw_x, 0)
+    min_y = np.where(dw_y < 0, dw_y, 0)
+    dphi_x, dphi_y = grad(phi, True)
+    dphi_x_f, dphi_y_f = grad_flipped(phi)
+
+    return (max_x * dphi_x + min_x * dphi_x_f +
+            max_y * dphi_y + min_y * dphi_y_f)
+
 
 def geodesic(gradient):
     """
@@ -83,6 +139,7 @@ def geodesic(gradient):
 
     # Return geodesic function
     return 1 / (magnitude_gradient + 1)
+
 
 if __name__ == '__main__':
     # Define number of steps
@@ -99,7 +156,6 @@ if __name__ == '__main__':
 
     # Compute w
     w = geodesic(grad(Im))
-    print("geodesic: ", w[150:300, 150:300])
 
     # Compute gradient of w
     dw = magnitude(grad(w))
@@ -107,12 +163,12 @@ if __name__ == '__main__':
     # Tau/Step size
     tau = 0.5
 
-    for t in range(1):
+    for t in range(n_steps):
         # Compute mean curvature motion
         curvature = compute_curvature(phi)
 
         # Compute propagation term
-        propagation = dw * magnitude(grad(phi))
+        propagation = compute_propagation(w, phi)
 
         # Update phi
         phi += tau * w * (curvature + propagation)
