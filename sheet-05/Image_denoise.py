d8fa690badd44ff36b82bd586ab27efd2691e05c
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-11-23 13:06:55 +0100

    Finished task 4

--- sheet-05/Image_denoise.py
+++ sheet-05/Image_denoise.py
@@ -1,6 +1,10 @@
 import cv2
+import imageio
 import numpy as np
 import maxflow
+import matplotlib.pyplot as plt
+from maxflow.fastmin import aexpansion_grid
+
 
 def question_3(I, rho=0.7, prw_same=0.005, prw_diff=0.2):
     ### 1) Define Graph
@@ -10,8 +14,8 @@ def question_3(I, rho=0.7, prw_same=0.005, prw_diff=0.2):
     nodeids = g.add_grid_nodes(I.shape)
 
     ### 3) Compute Unary cost
-    u_source = np.where(I == 0, -np.log(rho), -np.log(1-rho))
-    u_sink = np.where(I == 0, -np.log(1-rho), -np.log(rho))
+    u_source = np.where(I == 0, -np.log(rho), -np.log(1 - rho))
+    u_sink = np.where(I == 0, -np.log(1 - rho), -np.log(rho))
 
     ### 4) Add terminal edges
     # g.add_grid_tedges(nodeids, -np.log(rho), -np.log(1-rho))
@@ -25,9 +29,8 @@ def question_3(I, rho=0.7, prw_same=0.005, prw_diff=0.2):
 
     v_weights = np.zeros((I.shape[0], I.shape[1]))
     for col in range(I.shape[1]):
-        for row in range(I.shape[0]-1):
-            v_weights[row, col] = prw_same if I[row][col] == I[row+1][col] else prw_diff
-    print("Vertical weights: ", v_weights)
+        for row in range(I.shape[0] - 1):
+            v_weights[row, col] = prw_same if I[row][col] == I[row + 1][col] else prw_diff
 
     g.add_grid_edges(nodeids, weights=v_weights, structure=structure, symmetric=True)
 
@@ -38,9 +41,8 @@ def question_3(I, rho=0.7, prw_same=0.005, prw_diff=0.2):
 
     h_weights = np.zeros((I.shape[0], I.shape[1]))
     for row in range(I.shape[0]):
-        for col in range(I.shape[1]-1):
-            h_weights[row, col] = prw_same if I[row][col] == I[row][col+1] else prw_diff
-    print("horizontal weights: ", h_weights)
+        for col in range(I.shape[1] - 1):
+            h_weights[row, col] = prw_same if I[row][col] == I[row][col + 1] else prw_diff
 
     g.add_grid_edges(nodeids, weights=h_weights, structure=structure, symmetric=True)
 
@@ -50,36 +52,54 @@ def question_3(I, rho=0.7, prw_same=0.005, prw_diff=0.2):
     # Denoise image
     sgm = g.get_grid_segments(nodeids)
     Denoised_I = np.int_(np.logical_not(sgm)).astype(np.uint8) * 255
-    print("Segmented: ", Denoised_I)
 
     cv2.imshow('Original Img', I), \
     cv2.imshow('Denoised Img', Denoised_I), cv2.waitKey(0), cv2.destroyAllWindows()
 
-def question_4(I,rho=0.6):
 
-    labels = np.unique(I).tolist()
+def question_4(I, rho=0.8):
+    labels = np.unique(I)
 
-    Denoised_I = np.zeros_like(I)
-    ### Use Alpha expansion binary image for each label
+    # 1) Compute Unary cost
+    # define unary costs
+    u_same = - np.log10(rho)
+    u_diff = - np.log10((1 - rho) / 2)
 
-    ### 1) Define Graph
+    # assign unary costs
+    D = np.zeros(I.shape + labels.shape)
+    for y in range(I.shape[0]):
+        for x in range(I.shape[1]):
+            for lbl in range(labels.shape[0]):
+                if I[y, x] == labels[lbl]:
+                    D[y, x, lbl] = u_same
+                else:
+                    D[y, x, lbl] = u_diff
 
-    ### 2) Add pixels as nodes
+    # 2) Compute Pairwise cost metric
+    # define pairwise cost
+    def pairwise_cost(i, j):
+        def kronecker_delta(i, j):
+            return 1 if i == j else 0
 
-    ### 3) Compute Unary cost
+        return 1 - kronecker_delta(i, j)
 
-    ### 4) Add terminal edges
+    # assign pairwise costs
+    V = np.zeros((labels.shape[0], labels.shape[0]))
+    for i in range(labels.shape[0]):
+        for j in range(labels.shape[0]):
+            V[i, j] = pairwise_cost(i, j)
 
-    ### 5) Add Node edges
-    ### Vertical Edges
+    # 3) Alpha expansion
+    alpha_exp = aexpansion_grid(D, V).astype(np.uint8)
 
-    ### Horizontal edges
-    # (Keep in mind the stucture of neighbourhood and set the weights according to the pairwise potential)
-
-    ### 6) Maxflow
+    # 4) Assign values to labels
+    for lbl in range(labels.shape[0]):
+        alpha_exp[alpha_exp == lbl] = labels[lbl]
+    Denoised_I = alpha_exp
 
     cv2.imshow('Original Img', I), \
     cv2.imshow('Denoised Img', Denoised_I), cv2.waitKey(0), cv2.destroyAllWindows()
+
 
 def main():
     image_q3 = cv2.imread('./images/noise.png', cv2.IMREAD_GRAYSCALE)
@@ -91,7 +111,8 @@ def main():
     question_3(image_q3, rho=0.7, prw_same=0.005, prw_diff=0.55)
 
     ### Call solution for question 4
-    # question_4(image_q4, rho=0.8)
+    question_4(image_q4, rho=0.8)
+
 
 if __name__ == "__main__":
     main()
