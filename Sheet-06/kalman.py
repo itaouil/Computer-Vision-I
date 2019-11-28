b6b4943ad43da2d40b2358c958cfce5f1f6fe3e6
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-11-28 21:34:52 +0100

    Task 2 compleated

--- Sheet-06/kalman.py
+++ Sheet-06/kalman.py
@@ -14,22 +14,43 @@ class KalmanFilter(object):
         self.sigma_p = sigma_p
         self.phi = phi
         self.sigma_m = sigma_m
-        self.state = None
-        self.convariance = None
+        self.states = np.zeros((4, (tau + 1)))
+        self.cov = None
         self.tau = tau
 
     def init(self, init_state):
-        self.state = init_state
-        # self.covariance =
-        pass
+        self.states[:, 0] = init_state
+        self.cov = np.identity(4)
 
     def track(self, xt):
-        # to do
-        pass
+        xt = np.array([xt]).T
+        cov_p = self.compute_tau_pred()
+        self.compute_correction(cov_p, xt)
+
+        return False
+
+    def compute_tau_pred(self):
+        state_t_1 = self.states[:, 0]
+        state_t_p = np.dot(self.psi, state_t_1)
+        self.states = np.roll(self.states, 1)
+        self.states[:, 0] = state_t_p
+        cov_p = self.sigma_p + np.dot(np.dot(self.psi, self.cov), self.psi.T)
+        return cov_p
+
+    def compute_correction(self, cov_p, xt):
+        k_gain = np.dot(
+            np.dot(cov_p, self.phi.T),
+            # ----
+            np.linalg.inv(
+                (self.sigma_m + np.dot(np.dot(self.phi, cov_p), self.phi.T))
+            )
+        )
+
+        self.states = self.states + np.dot(k_gain, (xt - np.dot(self.phi, self.states)))
+        self.cov = np.dot((np.identity(4) - np.dot(k_gain, self.phi)), cov_p)
 
     def get_current_location(self):
-        # to do
-        pass
+        return self.states[:, -1]
 
 
 def perform_tracking(tracker):
@@ -37,12 +58,11 @@ def perform_tracking(tracker):
     for t in range(len(observations)):
         tracker.track(get_observation(t))
         track.append(tracker.get_current_location())
-
     return track
 
 
 def main():
-    init_state = np.array([1, 0, 0, 0])
+    init_state = np.array([0, 1, 0, 0])
 
     psi = np.array([[1, 0, 1, 0],
                     [0, 1, 0, 1],
@@ -63,15 +83,15 @@ def main():
     tracker = KalmanFilter(psi, sigma_p, phi, sigma_m, tau=0)
     tracker.init(init_state)
 
-    fixed_lag_smoother = KalmanFilter(psi, sigma_p, phi, sigma_m, tau=5)
+    fixed_lag_smoother = KalmanFilter(psi, sigma_p, phi, sigma_m, tau=25)
     fixed_lag_smoother.init(init_state)
 
     track = perform_tracking(tracker)
     track_smoothed = perform_tracking(fixed_lag_smoother)
 
     plt.figure()
-    plt.plot([x[0] for x in observations], [x[1] for x in observations])
-    plt.plot([x[0] for x in track], [x[1] for x in track])
+    # plt.plot([x[0] for x in observations], [x[1] for x in observations])
+    # plt.plot([x[0] for x in track], [x[1] for x in track])
     plt.plot([x[0] for x in track_smoothed], [x[1] for x in track_smoothed])
 
     plt.show()
