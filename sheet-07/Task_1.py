dbb35ad85f8209168c5c7a002a261b184e777473
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-12-03 16:15:37 +0100

    Troll template

---
+++ sheet-07/Task_1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib
+import cv2
+
+
+def task_1():
+    pass
+
+
+task_1()
---
+++ sheet-07/Task_2_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib
+import cv2
+
+def task_2():
+	pass
+
+def task_3():
+	pass
+
+task_2()
+
+task_3()
+
+
