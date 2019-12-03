90918e2a38b98938313fa125f18f64374a82b5a5
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-12-03 18:22:25 +0100

    Task 1, step 3 completed

--- sheet-07/Task_1.py
+++ sheet-07/Task_1.py
@@ -12,6 +12,7 @@ import cv2 as cv
   5) Compute next point using estimated Phi
 """
 
+
 def display_image(window_name, img):
     """
         Displays image with given window name.
@@ -19,27 +20,76 @@ def display_image(window_name, img):
     cv.imshow(window_name, img)
     cv.waitKey(0)
     cv.destroyAllWindows()
-  
-  
-def read_landmarks(path):
-  """
-    Reads landmarks points
-  """
-  landmarks = np.loadtxt("hand_landmarks.txt", dtype=np.str, delimiter=',')
+
+
+def read_landmarks(name):
+    """Read data from file.
+    """
+    landmarks = []
+    with open(name) as f:
+        for line in f:
+            line = line.strip()
+            line = line[1:-1]
+            landmarks.append(line.split(','))
+        landmarks_final = np.array(landmarks).astype(np.uint)
+
+    return landmarks_final
 
 
 def distance_transform(img):
-  """
-    Compute distance transform.
-  """
-  return cv.distanceTransform(img, cv2.DIST_L2, 5)
+    """
+      Compute distance transform.
+    """
+    return cv.distanceTransform(img, cv2.DIST_L2, 5)
+
+
+class IterClosePoint(object):
+
+    def __init__(self, img, landmarks, iterations=10):
+        self.img = img
+        self.landmarks = landmarks
+        self.iterations = iterations
+        self.B = landmarks.flatten()
+
+    def fit(self):
+        A = self.lands2A()
+        for iter in range(self.iterations):
+            print('Iteration: {}/{}'.format(iter + 1, self.iterations))
+
+            # step 3
+            Psi = self.get_affine(A)
+
+    def get_affine(self, A):
+        D, U, V_t = cv.SVDecomp(A)
+        B_p = np.dot(U.T, self.B)
+        B_p = np.array([B_p])
+        Y = B_p.T / D
+        X = np.dot(V_t.T, Y)
+        return X
+
+    def lands2A(self):
+        length = self.landmarks.shape[0]
+        A = np.zeros((2 * length, 6 * length))
+        for idx_land in range(length):
+            x, y = self.landmarks[idx_land]
+            idx_row = idx_land * 2
+            idx_col = idx_land * 6
+            A[idx_row, idx_col:idx_col + 6] = [x, y, 0, 0, 1, 0]
+            A[idx_row + 1, idx_col:idx_col + 6] = [0, 0, x, y, 0, 1]
+        return A
 
 
 def task_1():
-  """
-    Main.
-  """
+    """
+      Main.
+    """
+    iterations = 5
     img = cv.imread("./data/hand.jpg", 0)
+    landmarks = read_landmarks('./data/hand_landmarks.txt')
+
+    model = IterClosePoint(img, landmarks, iterations)
+    model.fit()
 
 
-task_1()
+if __name__ == '__main__':
+    task_1()
