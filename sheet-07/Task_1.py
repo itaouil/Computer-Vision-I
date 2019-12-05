06ce68df137adea24eaa72133b0f0aa7c0b1cc52
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-12-05 17:09:46 +0100

    Little changes of task 1

--- sheet-07/Task_1.py
+++ sheet-07/Task_1.py
@@ -8,18 +8,9 @@ import cv2 as cv
   1) Read landmarks.txt file with numpy
   2) Pre-compute distance transform for hand.jpg
   3) Compute ICP for pixel Xn
-  4) Estimate Phin for pixel Xn (SVD)
+  4) Estimate Phi for pixel Xn (SVD)
   5) Compute next point using estimated Phi
 """
-
-
-def display_image(window_name, img):
-    """
-        Displays image with given window name.
-    """
-    cv.imshow(window_name, img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 
 
 def read_landmarks(name):
@@ -37,6 +28,35 @@ def read_landmarks(name):
     return landmarks_final
 
 
+def plot_lands(img, lands, text, fill='green', line='red', alpha=1, with_txt=False):
+    """ plots the snake onto a sub-plot
+    :param V: point locations ( [ (x0, y0), (x1, y1), ... (xn, yn)]
+    :param fill: point color
+    :param line: line color
+    :param alpha: [0 .. 1]
+    :param with_txt: if True plot numbers as well
+    :return:
+    """
+    lands2d = np.reshape(lands, (lands.shape[0] // 2, 2))
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+
+    ax.clear()
+    ax.imshow(img, cmap='gray')
+    ax.set_title(text)
+
+    V_plt = np.append(lands2d.reshape(-1), lands2d[0, :]).reshape((-1, 2))
+    ax.plot(V_plt[:, 0], V_plt[:, 1], color=line, alpha=alpha)
+    ax.scatter(lands2d[:, 0], lands2d[:, 1], color=fill,
+               edgecolors='black',
+               linewidth=2, s=50, alpha=alpha)
+    if with_txt:
+        for i, (x, y) in enumerate(lands2d):
+            ax.text(x, y, str(i))
+
+    fig.show()
+
+
 class IterClosePoint(object):
 
     def __init__(self, img, landmarks, iterations=10):
@@ -44,10 +64,9 @@ class IterClosePoint(object):
         self.landmarks = landmarks
         self.iterations = iterations
         self.Psi = None
-        self.A = self.lands2A()
         self.B = np.array([landmarks.flatten()]).T
         self.DT = self.distance_transform()
-        # display_image('', self.DT.astype(np.uint8))
+        self.A = self.lands2A()
 
     def distance_transform(self):
         """
@@ -100,24 +119,20 @@ class IterClosePoint(object):
         """
             Compute ICP B_p
         """
-        # Vector of distances
-        # for each entry in B_p
+        # Vector of distances for each entry in B_p
         D = self.get_distances(B_p)
 
-        # Vector of gradients
-        # for each entry in B_p
+        # Vector of gradients for each entry in B_p
         G_x, G_y = self.get_gradients(B_p)
 
         # Compute Closest Points
-        # B_delta = (D * G_x * G_y)
-        B_delta = ((D / (np.sqrt(G_x ** 2 + G_y ** 2) + .00001)) * G_x * G_y)
-        CP = B_p - B_delta
+        CP = B_p - ((D / (np.sqrt(G_x ** 2 + G_y ** 2) + .00001)) * G_x * G_y)
 
         return np.around(CP)
 
     def fit(self):
         # Visualize the initial points.
-        self.plot_lands(self.B, 'Initial Shape')
+        plot_lands(self.img, self.B, 'Initial Shape')
 
         # Plot B
         for iter in range(self.iterations):
@@ -140,57 +155,19 @@ class IterClosePoint(object):
         if plot:
             # Visualize the landmark points
             # using the psi transformation.
-            A_stack = self.lands2A(stack=True)
+            A_stack = self.lands2A()
             B_p = np.dot(A_stack, self.Psi)
-            self.plot_lands(B_p, 'Shape after Psi transformation')
+            plot_lands(self.img, B_p, 'Shape after Psi transformation')
 
-    def lands2A(self, stack=True):
+    def lands2A(self):
         length = self.landmarks.shape[0]
-        A = None
-        if not stack:
-            A = np.zeros((2 * length, 6 * length))
-            for idx_land in range(length):
-                x, y = self.landmarks[idx_land]
-                idx_row = idx_land * 2
-                idx_col = idx_land * 6
-                A[idx_row, idx_col:idx_col + 6] = [x, y, 0, 0, 1, 0]
-                A[idx_row + 1, idx_col:idx_col + 6] = [0, 0, x, y, 0, 1]
-        else:
-            A = np.zeros((2 * length, 6))
-            for idx_land in range(length):
-                x, y = self.landmarks[idx_land]
-                idx_row = idx_land * 2
-                A[idx_row, :] = [x, y, 0, 0, 1, 0]
-                A[idx_row + 1, :] = [0, 0, x, y, 0, 1]
+        A = np.zeros((2 * length, 6))
+        for idx_land in range(length):
+            x, y = self.landmarks[idx_land]
+            idx_row = idx_land * 2
+            A[idx_row, :] = [x, y, 0, 0, 1, 0]
+            A[idx_row + 1, :] = [0, 0, x, y, 0, 1]
         return A
-
-    def plot_lands(self, lands, text, fill='green', line='red', alpha=1, with_txt=False):
-        """ plots the snake onto a sub-plot
-        :param V: point locations ( [ (x0, y0), (x1, y1), ... (xn, yn)]
-        :param fill: point color
-        :param line: line color
-        :param alpha: [0 .. 1]
-        :param with_txt: if True plot numbers as well
-        :return:
-        """
-        lands2d = np.reshape(lands, (lands.shape[0] // 2, 2))
-        fig = plt.figure(figsize=(10, 10))
-        ax = fig.add_subplot(111)
-
-        ax.clear()
-        ax.imshow(self.img, cmap='gray')
-        ax.set_title(text)
-
-        V_plt = np.append(lands2d.reshape(-1), lands2d[0, :]).reshape((-1, 2))
-        ax.plot(V_plt[:, 0], V_plt[:, 1], color=line, alpha=alpha)
-        ax.scatter(lands2d[:, 0], lands2d[:, 1], color=fill,
-                   edgecolors='black',
-                   linewidth=2, s=50, alpha=alpha)
-        if with_txt:
-            for i, (x, y) in enumerate(lands2d):
-                ax.text(x, y, str(i))
-
-        fig.show()
 
 
 def task_1():
@@ -200,8 +177,6 @@ def task_1():
     iterations = 5
     img = cv.imread("./data/hand.jpg", 0)
     landmarks = read_landmarks('./data/hand_landmarks.txt')
-    # img = cv.imread("/Users/dailand10/Desktop/Computer-Vision-I/sheet-07/data/hand.jpg", 0)
-    # landmarks = read_landmarks('/Users/dailand10/Desktop/Computer-Vision-I/sheet-07/data/hand_landmarks.txt')
 
     model = IterClosePoint(img, landmarks, iterations)
     model.fit()
