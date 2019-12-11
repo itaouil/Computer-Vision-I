3561b1ebab753b2b9938a2485db5eec7236b8484
Alessandro Riccardi <alericcardi1996@gmail.com>
2019-12-11 20:15:30 +0100

    Finished task 2

--- sheet-08/exercise2.py
+++ sheet-08/exercise2.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
-from sklearn.cluster import MeanShift
+
 
 def display_image(window_name, img):
     """
@@ -13,12 +13,13 @@ def display_image(window_name, img):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+
 def get_structural_tensor(img, ksize):
     """
         Compute M for img.
     """
     # Smooth image
-    img = cv2.GaussianBlur(img, (3,3), 1)
+    img = cv2.GaussianBlur(img, (3, 3), 1)
 
     # Compute derivatives for img
     kernel_x = np.array([[-1, 1]])
@@ -42,9 +43,14 @@ def get_structural_tensor(img, ksize):
     # Populate matrix M
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
-            M[y,x,:] = np.array([I_xx[y,x], I_xy[y,x], I_xy[y,x], I_yy[y,x]])
-    
+            M[y, x, :] = np.array([I_xx[y, x], I_xy[y, x], I_xy[y, x], I_yy[y, x]])
+
     return M
+
+
+def local_maxima(img):
+    cv2.meanShift(img, 20, criteria)
+
 
 def harris_detector(M):
     """
@@ -58,38 +64,64 @@ def harris_detector(M):
         for x in range(M.shape[1]):
             # Reshape array into
             # a 2x2 matrix
-            M_temp = np.reshape(M[y,x], (2,2))
+            M_temp = np.reshape(M[y, x], (2, 2))
 
             # Compute f value
-            responses[y,x] = np.linalg.det(M_temp) - 0.04 * np.trace(M_temp) ** 2
+            responses[y, x] = np.linalg.det(M_temp) - 0.04 * np.trace(M_temp) ** 2
 
     # Threshold responses
     responses = np.where(responses > 100, 255, 0)
 
-    # Apply mean shift
-    clustering = MeanShift(bandwidth=2).fit(responses)
-
-    responses = np.reshape(clustering.labels_, responses.shape)
+    # responses = img_mean_shift(responses)
 
     return responses
 
 
+def forstner_detector(M):
+    """
+        Compute Forstner responses.
+    """
+    # Forstner responses
+    w = np.zeros((M.shape[0], M.shape[1]))
+    q = np.zeros((M.shape[0], M.shape[1]))
+
+    # Compute responses for each pixel
+    for y in range(M.shape[0]):
+        for x in range(M.shape[1]):
+            # Reshape array into
+            # a 2x2 matrix
+            M_temp = np.reshape(M[y, x], (2, 2))
+
+            # Compute f value
+            w[y, x] = np.linalg.det(M_temp) / (np.trace(M_temp) + .00001)
+            q[y, x] = 4 * np.linalg.det(M_temp) / (np.trace(M_temp) ** 2 + .00001)
+
+    q = (q - q.min()) / (q.max() - q.min()) * 255
+
+    # Threshold responses
+    w = np.where(w > 100, 255, 0)
+    q = np.where(q > 254.99995, 255, 0)
+
+    return w, q
+
+
 def main():
     # Load the image
-    # img = cv2.imread("./data/exercise2/building.jpeg", 0)
-    img = cv2.imread("/Users/dailand10/Desktop/Computer-Vision-I/sheet-08/data/exercise2/building.jpeg", 0)
+    img = cv2.imread("./data/exercise2/building.jpeg", 0)
+    # img = cv2.imread("/Users/dailand10/Desktop/Computer-Vision-I/sheet-08/data/exercise2/building.jpeg", 0)
 
     # Compute Structural Tensor
-    ksize = (3,3)
+    ksize = (3, 3)
     M = get_structural_tensor(img, ksize)
 
     # Harris Corner Detection
     corners = harris_detector(M)
-    display_image("Corners", corners.astype(np.uint8))
+    display_image("Harris' corners", corners.astype(np.uint8))
 
     # Forstner Corner Detection
-
-    pass
+    w, q = forstner_detector(M)
+    display_image("Forstner's corners w", w.astype(np.uint8))
+    display_image("Forstner's corners q", q.astype(np.uint8))
 
 
 if __name__ == '__main__':
