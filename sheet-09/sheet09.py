21a22e143abcf661caa1f2c5fd2ba2322014e667
Ilyass Taouil <itaouil95@gmail.com>
2019-12-17 17:16:33 +0100

    New sheet.

---
+++ sheet-09/sheet09.py
@@ -0,0 +1,139 @@
+import numpy as np
+import os
+import time
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+
+def load_FLO_file(filename):
+    assert os.path.isfile(filename), 'file does not exist: ' + filename   
+    flo_file = open(filename,'rb')
+    magic = np.fromfile(flo_file, np.float32, count=1)
+    assert magic == 202021.25,  'Magic number incorrect. .flo file is invalid'
+    w = np.fromfile(flo_file, np.int32, count=1)
+    h = np.fromfile(flo_file, np.int32, count=1)
+    data = np.fromfile(flo_file, np.float32, count=2*w[0]*h[0])
+    flow = np.resize(data, (int(h[0]), int(w[0]), 2))
+    flo_file.close()
+    return flow
+
+class OpticalFlow:
+    def __init__(self):
+        # Parameters for Lucas_Kanade_flow()
+        self.EIGEN_THRESHOLD = 0.01 # use as threshold for determining if the optical flow is valid when performing Lucas-Kanade
+        self.WINDOW_SIZE = [25, 25] # the number of points taken in the neighborhood of each pixel
+
+        # Parameters for Horn_Schunck_flow()
+        self.EPSILON= 0.002 # the stopping criterion for the difference when performing the Horn-Schuck algorithm
+        self.MAX_ITERS = 1000 # maximum number of iterations allowed until convergence of the Horn-Schuck algorithm
+        self.ALPHA = 1.0 # smoothness term
+
+        # Parameter for flow_map_to_bgr()
+        self.UNKNOWN_FLOW_THRESH = 1000
+
+        self.prev = None
+        self.next = None
+
+    def next_frame(self, img):
+        self.prev = self.next
+        self.next = img
+
+        if self.prev is None:
+            return False
+
+        frames = np.float32(np.array([self.prev, self.next]))
+        frames /= 255.0
+
+        #calculate image gradient
+        self.Ix = cv.Sobel(frames[0], cv.CV_32F, 1, 0, 3)
+        self.Iy = cv.Sobel(frames[0], cv.CV_32F, 0, 1, 3)
+        self.It = frames[1]-frames[0]
+
+        return True
+
+    #***********************************************************************************
+    # function for converting flow map to to BGR image for visualisation
+    # return bgr image
+    def flow_map_to_bgr(self, flow):
+        flow_bgr = None
+
+        return flow_bgr
+
+    #***********************************************************************************
+    # implement Lucas-Kanade Optical Flow 
+    # returns the Optical flow based on the Lucas-Kanade algorithm and visualisation result
+    def Lucas_Kanade_flow(self):
+        flow = None
+
+        flow_bgr = self.flow_map_to_bgr(flow)
+        return flow, flow_bgr
+
+    #***********************************************************************************
+    # implement Horn-Schunck Optical Flow 
+    # returns the Optical flow based on the Horn-Schunck algorithm and visualisation result
+    def Horn_Schunck_flow(self):
+        flow = None
+
+        flow_bgr = self.flow_map_to_bgr(flow)
+        return flow, flow_bgr
+
+    #***********************************************************************************
+    #calculate the angular error here
+    # return average angular error and per point error map
+    def calculate_angular_error(self, estimated_flow, groundtruth_flow):
+        aae = None
+        aae_per_point = None
+
+        return aae, aae_per_point
+
+
+if __name__ == "__main__":
+
+    data_list = [
+        'data/frame_0001.png',
+        'data/frame_0002.png',
+        'data/frame_0007.png',
+    ]
+
+    gt_list = [
+        './data/frame_0001.flo',
+        './data/frame_0002.flo',
+        './data/frame_0007.flo',
+    ]
+
+    Op = OpticalFlow()
+    
+    for (i, (frame_filename, gt_filemane)) in enumerate(zip(data_list, gt_list)):
+        groundtruth_flow = load_FLO_file(gt_filemane)
+        img = cv.cvtColor(cv.imread(frame_filename), cv.COLOR_BGR2GRAY)
+        if not Op.next_frame(img):
+            continue
+
+        flow_lucas_kanade, flow_lucas_kanade_bgr = Op.Lucas_Kanade_flow()
+        aae_lucas_kanade, aae_lucas_kanade_per_point = Op.calculate_angular_error(flow_lucas_kanade, groundtruth_flow)
+        print('Average Angular error for Luacas-Kanade Optical Flow: %.4f' %(aae_lucas_kanade))
+
+        flow_horn_schunck, flow_horn_schunck_bgr = Op.Horn_Schunck_flow()
+        aae_horn_schunk, aae_horn_schunk_per_point = Op.calculate_angular_error(flow_horn_schunck, groundtruth_flow)        
+        print('Average Angular error for Horn-Schunck Optical Flow: %.4f' %(aae_horn_schunk))   
+
+        flow_bgr_gt = Op.flow_map_to_bgr(groundtruth_flow)
+
+        fig = plt.figure(figsize=(img.shape))
+
+        # Display
+        fig.add_subplot(2, 3, 1)
+        plt.imshow(flow_bgr_gt)
+        fig.add_subplot(2, 3, 2)
+        plt.imshow(flow_lucas_kanade_bgr)
+        fig.add_subplot(2, 3, 3)
+        plt.imshow(aae_lucas_kanade_per_point)
+        fig.add_subplot(2, 3, 4)
+        plt.imshow(flow_bgr_gt)
+        fig.add_subplot(2, 3, 5)
+        plt.imshow(flow_horn_schunck_bgr)
+        fig.add_subplot(2, 3, 6)
+        plt.imshow(aae_horn_schunk_per_point)
+        plt.show()
+
+        print("*"*20)
