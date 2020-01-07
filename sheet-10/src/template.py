0b5e994118171f0f82f784a7425809e09ed26782
Alessandro Riccardi <alericcardi1996@gmail.com>
2020-01-07 18:32:58 +0100

    Task 2 compleated

--- sheet-10/src/template.py
+++ sheet-10/src/template.py
@@ -4,10 +4,10 @@ import os
 import os.path as osp
 import matplotlib.pyplot as plt
 
-NUM_IMAGES=14
+NUM_IMAGES = 14
 NUM_Boards = NUM_IMAGES
-# image_prefix = "../images/"
-image_prefix = "/Users/dailand10/Desktop/Computer-Vision-I/sheet10/images/"
+image_prefix = "images/"
+# image_prefix = "/Users/dailand10/Desktop/Computer-Vision-I/sheet10/images/"
 image_suffix = ".png"
 images_files_list = [osp.join(image_prefix, f) for f in os.listdir(image_prefix)
                      if osp.isfile(osp.join(image_prefix, f)) and f.endswith(image_suffix)]
@@ -15,20 +15,22 @@ board_w = 10
 board_h = 7
 board_size = (board_w, board_h)
 board_n = board_w * board_h
-img_shape = (0,0)
+img_shape = (0, 0)
 obj = []
 for ptIdx in range(0, board_n):
-    obj.append(np.array([[ptIdx/board_w, ptIdx%board_w, 0.0]], np.float32))
+    obj.append(np.array([[ptIdx / board_w, ptIdx % board_w, 0.0]], np.float32))
 obj = np.vstack(obj)
+
 
 def task1():
     """
         Detect and display
         corners for each image
     """
+    global img_shape
     # Arrays to store object points and image points from all the images.
-    objpoints = [] # 3d point in real world space
-    imgpoints = [] # 2d points in image plane.
+    objpoints = []  # 3d point in real world space
+    imgpoints = []  # 2d points in image plane.
 
     # termination criteria
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -38,9 +40,9 @@ def task1():
         # Read image as grayscale
         img = cv2.imread(img_file)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+        img_shape = gray.shape
         # Find the chessboard corners
-        found, corners = cv2.findChessboardCorners(gray, (board_h, board_w), None)
+        found, corners = cv2.findChessboardCorners(gray, (board_w, board_h), None)
 
         # Check if corners found
         # and display them
@@ -49,51 +51,62 @@ def task1():
             objpoints.append(obj)
 
             # Refine detected corner
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
             imgpoints.append(corners2)
 
             # Draw and display the corners
-            # img = cv2.drawChessboardCorners(img, (board_h, board_w), corners2, found)
+            # img = cv2.drawChessboardCorners(img, (board_w, board_h), corners2, found)
             # cv2.imshow('img', img)
             # cv2.waitKey(500)
-    
+
     return imgpoints, objpoints
 
+
 def task2(imagePoints, objectPoints):
-    #implement your solution
-    pass
+    ret, CM, D, rvecs, tvecs = cv2.calibrateCamera(objectPoints, imagePoints, img_shape[::-1], None, None)
+    print('cameraMatrix:\n', CM, end='\n\n')
+    print('distortionMatrix:\n', D, end='\n\n')
+    print('rotation:\n', rvecs, end='\n\n')
+    print('translation:\n', tvecs, end='\n\n')
+    return CM, D, rvecs, tvecs
+
 
 def task3(imagePoints, objectPoints, CM, D, rvecs, tvecs):
-    #implement your solution
+    # implement your solution
     pass
+
 
 def task4(CM, D):
-    #implement your solution
+    # implement your solution
     pass
+
 
 def task5(CM, rvecs, tvecs):
-    #implement your solution
+    # implement your solution
     pass
 
+
 def main():
-    #Showing images
+    # Showing images
     # for img_file in images_files_list:
     #     print(img_file)
     #     img = cv2.imread(img_file)
+    #     images.append(img)
     #     cv2.imshow("Task1", img)
     #     cv2.waitKey(10)
-    
-    imagePoints, objectPoints = task1() #Calling Task 1
-    
-    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints) #Calling Task 2
+
+    imagePoints, objectPoints = task1()  # Calling Task 1
+
+    CM, D, rvecs, tvecs = task2(imagePoints, objectPoints)  # Calling Task 2
 
     task3(imagePoints, objectPoints, CM, D, rvecs, tvecs)  # Calling Task 3
 
-    task4(CM, D) # Calling Task 4
+    task4(CM, D)  # Calling Task 4
 
-    task5(CM, rvecs, tvecs) # Calling Task 5
-    
+    task5(CM, rvecs, tvecs)  # Calling Task 5
+
     print("FINISH!")
+
 
 if __name__ == "__main__":
     main()
